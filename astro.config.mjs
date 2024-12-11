22dbb2984f5525d5ad46dc33fba035f6cefd3627
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2024-12-10 19:48:05 -0600

    New portfolio design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://eduardoconx.github.io/'
+  integrations: [tailwind(), react()]
 });
---
+++ src/interfaces/index.ts
@@ -0,0 +1,16 @@
+export interface Star {
+  x: number;
+  y: number;
+  size: number;
+  opacity: number;
+}
+
+export interface Meteor {
+  x: number;
+  y: number;
+  length: number;
+  opacity: number;
+  angle: number;
+  speed: number;
+  size: number;
+}
